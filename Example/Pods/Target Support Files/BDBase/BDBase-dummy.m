677f7d0e88c8b8cd2bf09bee8f1b5b2c2efd6ebb
bandianhong <scottban@126.com>
2020-03-13 19:55:31 +0800

    Release 1.0.0

---
+++ Example/Pods/Target Support Files/BDBase/BDBase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BDBase : NSObject
+@end
+@implementation PodsDummy_BDBase
+@end
---
+++ Example/Pods/Target Support Files/Pods-BDBase_Tests/Pods-BDBase_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BDBase_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BDBase_Tests
+@end
